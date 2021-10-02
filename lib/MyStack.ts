006d82b7d68a2c12ad751167c3451804969953b1
Hamed <hamed@northone.com>
2021-10-02 19:28:45 -0400

    add 7 api gateway

--- lib/MyStack.ts
+++ lib/MyStack.ts
@@ -5,15 +5,78 @@ export default class MyStack extends sst.Stack {
     super(scope, id, props);
 
     // Create a HTTP API
-    const api = new sst.Api(this, "Api", {
+    const apiOne = new sst.Api(this, "ApiOne", {
       routes: {
-        "GET /": "src/lambda.handler",
+        "GET /": "src/lambda-one.handler",
+      },
+    });
+
+    const apiTwo = new sst.Api(this, "ApiTwo", {
+      routes: {
+        "GET /": "src/lambda-two.handler",
+      },
+    });
+
+    const apiThree = new sst.Api(this, "ApiThree", {
+      routes: {
+        "GET /": "src/lambda-three.handler",
+      },
+    });
+
+    const apiFour = new sst.Api(this, "ApiFour", {
+      routes: {
+        "GET /": "src/lambda-four.handler",
+      },
+    });
+
+    const apiFive = new sst.Api(this, "ApiFive", {
+      routes: {
+        "GET /": "src/lambda-five.handler",
+      },
+    });
+
+    const apiSix = new sst.Api(this, "ApiSix", {
+      routes: {
+        "GET /": "src/lambda-six.handler",
+      },
+    });
+
+    const apiSeven = new sst.Api(this, "ApiSeven", {
+      routes: {
+        "GET /": "src/lambda-seven.handler",
+      },
+    });
+
+    const apiEight = new sst.Api(this, "ApiEight", {
+      routes: {
+        "GET /": "src/lambda-eight.handler",
       },
     });
 
     // Show the endpoint in the output
     this.addOutputs({
-      "ApiEndpoint": api.url,
+      "ApiEndpoint": apiOne.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiTwo.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiThree.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiFour.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiFive.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiSix.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiSeven.url,
+    });
+    this.addOutputs({
+      "ApiEndpoint": apiEight.url,
     });
   }
 }

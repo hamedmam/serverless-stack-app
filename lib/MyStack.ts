40eff47838a0185da2b71842c03d094461412e33
Hamed <hamed@northone.com>
2021-10-02 22:24:10 -0400

    add table

--- lib/MyStack.ts
+++ lib/MyStack.ts
@@ -1,82 +1,82 @@
-import * as sst from "@serverless-stack/resources";
+import * as sst from '@serverless-stack/resources'
+import * as sfn from '@aws-cdk/aws-stepfunctions'
+import * as tasks from '@aws-cdk/aws-stepfunctions-tasks'
+import * as cdk from '@aws-cdk/core'
+import { TableFieldType } from '@serverless-stack/resources'
 
 export default class MyStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // Create a HTTP API
-    const apiOne = new sst.Api(this, "ApiOne", {
-      routes: {
-        "GET /": "src/lambda-one.handler",
+    const myTable = new sst.Table(this, 'MyTable', {
+      primaryIndex: { partitionKey: 'RequestId' },
+      fields: {
+        RequestId: TableFieldType.STRING,
       },
-    });
+    })
 
-    const apiTwo = new sst.Api(this, "ApiTwo", {
-      routes: {
-        "GET /": "src/lambda-two.handler",
-      },
-    });
+    const submitLambda = new sst.Function(this, 'SubmitLambda', {
+      runtime: 'nodejs12.x',
+      handler: 'src/lambda.handler',
+    })
 
-    const apiThree = new sst.Api(this, "ApiThree", {
-      routes: {
-        "GET /": "src/lambda-three.handler",
-      },
-    });
+    const getStatusLambda = new sst.Function(this, 'StatusLambda', {
+      runtime: 'nodejs12.x',
+      handler: 'src/get-status.handler',
+    })
 
-    const apiFour = new sst.Api(this, "ApiFour", {
-      routes: {
-        "GET /": "src/lambda-four.handler",
-      },
-    });
+    // const submitLambda = new sst.Function(this, 'SubmitLambda', {
 
-    const apiFive = new sst.Api(this, "ApiFive", {
-      routes: {
-        "GET /": "src/lambda-five.handler",
-      },
-    });
+    // })
+    // const getStatusLambda = new sst.Function(this, 'CheckLambda', {})
 
-    const apiSix = new sst.Api(this, "ApiSix", {
-      routes: {
-        "GET /": "src/lambda-six.handler",
-      },
-    });
+    // const submitJob = new tasks.LambdaInvoke(this, 'Submit Job', {
+    //   lambdaFunction: submitLambda,
+    //   // Lambda's result is in the attribute `Payload`
+    //   outputPath: '$.Payload',
+    // })
 
-    const apiSeven = new sst.Api(this, "ApiSeven", {
-      routes: {
-        "GET /": "src/lambda-seven.handler",
-      },
-    });
+    // const waitX = new sfn.Wait(this, 'Wait X Seconds', {
+    //   time: sfn.WaitTime.secondsPath('$.waitSeconds'),
+    // })
 
-    const apiEight = new sst.Api(this, "ApiEight", {
-      routes: {
-        "GET /": "src/lambda-eight.handler",
-      },
-    });
+    // const getStatus = new tasks.LambdaInvoke(this, 'Get Job Status', {
+    //   lambdaFunction: getStatusLambda,
+    //   // Pass just the field named "guid" into the Lambda, put the
+    //   // Lambda's result in a field called "status" in the response
+    //   inputPath: '$.guid',
+    //   outputPath: '$.Payload',
+    // })
 
-    // Show the endpoint in the output
-    this.addOutputs({
-      "ApiEndpointOne": apiOne.url,
-    });
-    this.addOutputs({
-      "ApiEndpointTwo": apiTwo.url,
-    });
-    this.addOutputs({
-      "ApiEndpointThree": apiThree.url,
-    });
-    this.addOutputs({
-      "ApiEndpointFour": apiFour.url,
-    });
-    this.addOutputs({
-      "ApiEndpointFive": apiFive.url,
-    });
-    this.addOutputs({
-      "ApiEndpointSix": apiSix.url,
-    });
-    this.addOutputs({
-      "ApiEndpointSeven": apiSeven.url,
-    });
-    this.addOutputs({
-      "ApiEndpointEight": apiEight.url,
-    });
+    // const jobFailed = new sfn.Fail(this, 'Job Failed', {
+    //   cause: 'AWS Batch Job Failed',
+    //   error: 'DescribeJob returned FAILED',
+    // })
+
+    // const finalStatus = new tasks.LambdaInvoke(this, 'Get Final Job Status', {
+    //   lambdaFunction: getStatusLambda,
+    //   // Use "guid" field as input
+    //   inputPath: '$.guid',
+    //   outputPath: '$.Payload',
+    // })
+
+    // const definition = submitJob
+    //   .next(waitX)
+    //   .next(getStatus)
+    //   .next(
+    //     new sfn.Choice(this, 'Job Complete?')
+    //       // Look at the "status" field
+    //       .when(sfn.Condition.stringEquals('$.status', 'FAILED'), jobFailed)
+    //       .when(
+    //         sfn.Condition.stringEquals('$.status', 'SUCCEEDED'),
+    //         finalStatus
+    //       )
+    //       .otherwise(waitX)
+    //   )
+
+    // new sfn.StateMachine(this, 'StateMachine', {
+    //   definition,
+    //   timeout: cdk.Duration.minutes(5),
+    // })
   }
 }
